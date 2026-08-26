d5262b63cc7decc068f88ff674c0397dc73f7cb1
ilxzuuceinn <nadia.geo.pantoja@gmail.com>
2026-08-26 13:22:28 -0600

    nose

--- codigo.js
+++ codigo.js
@@ -8,13 +8,8 @@ function registrar() {
         mensaje.textContent = "Completa todos los campos";
         mensaje.style.color = "red";
     } else {
-        localStorage.setItem("usuario", usuario);
-        localStorage.setItem("contrasena", contrasena);
         mensaje.textContent = "¡Registro exitoso!";
         mensaje.style.color = "purple";
-        setTimeout(function() {
-            location.href = "index.html";
-        }, 1000);
     }
 }
 
@@ -23,8 +18,6 @@ function iniciarSesion() {
     let usuario = document.getElementById("usuario").value;
     let contrasena = document.getElementById("contrasena").value;
     let mensaje = document.getElementById("mensaje");
-    let usuarioRegistrado = localStorage.getItem("usuario");
-    let contrasenaRegistrada = localStorage.getItem("contrasena");
     if (usuario == "" || contrasena == "") {
         mensaje.textContent = "Ingresa tu usuario y contraseña";
         mensaje.style.color = "red";
@@ -33,9 +26,6 @@ function iniciarSesion() {
     else if (usuario == usuarioRegistrado && contrasena == contrasenaRegistrada) {
         mensaje.textContent = "¡Inicio de sesión exitoso!";
         mensaje.style.color = "purple";
-        setTimeout(function() {
-            location.href = "bienvenido.html";
-        }, 1000);
     } 
 
     else {
