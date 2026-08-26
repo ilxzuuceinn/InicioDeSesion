512924de6aed4c3467968b8bf65be91812beec63
ilxzuuceinn <nadia.geo.pantoja@gmail.com>
2026-08-26 13:18:52 -0600

    cambio indentado

--- codigo.js
+++ codigo.js
@@ -14,10 +14,8 @@ function registrar() {
 
         localStorage.setItem("usuario", usuario);
         localStorage.setItem("contrasena", contrasena);
-
         mensaje.textContent = "¡Registro exitoso!";
         mensaje.style.color = "purple";
-
         setTimeout(function() {
             location.href = "index.html";
         }, 1000);
@@ -29,33 +27,23 @@ function iniciarSesion() {
 
     let usuario = document.getElementById("usuario").value;
     let contrasena = document.getElementById("contrasena").value;
-
     let mensaje = document.getElementById("mensaje");
-
-    
     let usuarioRegistrado = localStorage.getItem("usuario");
     let contrasenaRegistrada = localStorage.getItem("contrasena");
-
     if (usuario == "" || contrasena == "") {
-
         mensaje.textContent = "Ingresa tu usuario y contraseña";
         mensaje.style.color = "red";
-
     } 
    
     else if (usuario == usuarioRegistrado && contrasena == contrasenaRegistrada) {
-
         mensaje.textContent = "¡Inicio de sesión exitoso!";
         mensaje.style.color = "purple";
-
         setTimeout(function() {
             location.href = "bienvenido.html";
         }, 1000);
-
     } 
-    
-    else {
 
+    else {
         mensaje.textContent = "Usuario o contraseña incorrectos";
         mensaje.style.color = "red";
     }
