628825eecc4b9dfeb131f15929afaa062cd4fb96
ilxzuuceinn <nadia.geo.pantoja@gmail.com>
2026-08-31 09:07:30 -0600

    texto

--- codigo.js
+++ codigo.js
@@ -1,13 +1,32 @@
+const usuarios = [];
+
 function registrar() {
-    let usuario = document.getElementById("nuevoUsuario").value;
-    let contrasena = document.getElementById("nuevaContrasena").value;
+    let user = document.getElementById("nuevoUsuario").value;
+    let contra = document.getElementById("nuevaContrasena").value;
     let mensaje = document.getElementById("mensaje");
-    
-    
-    if (usuario == "" || contrasena == "") {
-        mensaje.textContent = "¡Completa todo!";
+
+    if (user === "" || contra === "") {
+        mensaje.textContent = "¡Completa todos los campos!";
+        mensaje.style.color = "red";
+    } 
+
+    let yaExiste = false;
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].usuario === user) {
+            yaExiste = true;
+            break;
+        }
+    }
+
+    if (yaExiste) {
+        mensaje.textContent = "¡El nombre de usuario ya está registrado!";
         mensaje.style.color = "red";
     } else {
+        usuarios.push({ 
+            usuario: user, 
+            contrasena: contra 
+        });
+        
         mensaje.textContent = "¡Registro exitoso!";
         mensaje.style.color = "purple";
     }
@@ -15,21 +34,27 @@ function registrar() {
 
 
 function iniciarSesion() {
-    let usuario = document.getElementById("usuario").value;
-    let contrasena = document.getElementById("contrasena").value;
+    let user = document.getElementById("usuario").value;
+    let contra = document.getElementById("contrasena").value;
     let mensaje = document.getElementById("mensaje");
-    if (usuario == "" || contrasena == "") {
+    if (user === "" || contra === "") {
         mensaje.textContent = "¡Ingresa tu usuario y contraseña!";
         mensaje.style.color = "red";
     } 
-   
-    else if (usuario == usuarioRegistrado && contrasena == contrasenaRegistrada) {
+
+    let opcionesValidas = false;
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].usuario === user && usuarios[i].contrasena === contra) {
+            opcionesValidas = true;
+            break;
+        }
+    }
+
+    if (opcionesValidas) {
         mensaje.textContent = "¡Inicio de sesión exitoso!";
         mensaje.style.color = "purple";
-    } 
-
-    else {
-        mensaje.textContent = "Usuario o contraseña incorrectos";
+    } else {
+        mensaje.textContent = "¡Usuario o contraseña incorrectos!";
         mensaje.style.color = "red";
     }
 }
